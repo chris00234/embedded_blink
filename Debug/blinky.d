a69145594cb75102d3b17e87e512953c5f986072
Chris <wheogus98@gmail.com>
2024-01-19 12:10:18 -0800

    first commit

---
+++ Debug/blinky.d
@@ -0,0 +1,27 @@
+# FIXED
+
+blinky.obj: ../blinky.c
+blinky.obj: /Applications/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdio.h
+blinky.obj: /Applications/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+blinky.obj: /Applications/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+blinky.obj: /Applications/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdarg.h
+blinky.obj: /Applications/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+blinky.obj: /Applications/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+blinky.obj: /Applications/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+
+../blinky.c:
+
+/Applications/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdio.h:
+
+/Applications/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+/Applications/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+/Applications/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdarg.h:
+
+/Applications/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+/Applications/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+/Applications/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
---
+++ blinky.c
@@ -0,0 +1,44 @@
+/*
+ * main.c
+ *
+ *  Created on: Jan 12, 2024
+ *      Author: chris
+ */
+
+#include <stdio.h>
+
+#define GPIODATA_F (*((volatile unsigned long *) 0x40025010)) // PF starts at 0x40025000 and our bit masking code is 0x010
+//GPIODATA_F = 0x00000100; // go inside of main
+
+#define GPIODIR (*((volatile unsigned long *) 0x40025400)) //pin direction control for port F with offset 0x400
+//GPIODIR = 0x00000100;
+
+#define GPIODEN (*((volatile unsigned long *) 0x4002551C)) // digital enable for port F with offset 0x51C
+//GPIODEN = 0x00000100;
+
+#define RCGCGPIO (*((volatile unsigned long *) 0x400FE608)) // starting address for clock gate is 0x400FE000 and offset of 0x608
+//RCGCGPIO = 0x00000100;
+
+#define blue_led = 0x04 // 0000.0100
+#define GPIO_PORTF_CLK_EN 0x20
+#define GPIO_PORTF_PIN2_EN  0x04
+
+void delay()
+{
+    volatile unsigned long i;
+    for (i = 0; i < 4000000; i++);
+}
+
+int main(void){
+    RCGCGPIO |= GPIO_PORTF_CLK_EN; // Enable clock for PORTF
+    GPIODEN |= GPIO_PORTF_PIN2_EN; // Enable pin 2 on PORTF
+    GPIODIR |= GPIO_PORTF_PIN2_EN; // Set pin 2 as an output
+
+    while(1){
+        GPIODATA_F = 0x04;
+        delay();
+        GPIODATA_F = 0x00;
+        delay();
+    }
+
+}
